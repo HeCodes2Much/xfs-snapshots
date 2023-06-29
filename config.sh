5dd1bb9c7be9431386e79d9e2b2a35c36c0e01c7
The-Repo-Club <wayne6324@gmail.com>
2023-06-29 14:44:59 +0100

    Added a config function.
    Signed-off-by: The-Repo-Club <wayne6324@gmail.com>

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# shellcheck disable=all
+
+# Declare variables
+max_backups=10
+backup_directory="/mnt/500GB/example"
+xfs_device="/dev/sdb1"
+target_directory="/mnt/500GB/example2"
